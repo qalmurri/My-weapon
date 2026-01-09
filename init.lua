b42ae5f5065f3db348d09c29c3852515b6e0a072
qalmurri <86642474+qalmurri@users.noreply.github.com>
2026-01-09 18:47:15 +0700

    Add files via upload

--- init.lua
+++ init.lua
@@ -1,52 +1,173 @@
--- =========================
--- Bootstrap lazy.nvim
--- =========================
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+-- ==========================================================================
+-- DISABLE UNUSED PROVIDERS (AMAN)
+-- ==========================================================================
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
 
+
+-- ==========================================================================
+-- LAZY.NVIM BOOTSTRAP
+-- ==========================================================================
+vim.g.mapleader = " "
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
+    "git", "clone", "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
     "--branch=stable",
     lazypath,
   })
 end
-
 vim.opt.rtp:prepend(lazypath)
 
--- =========================
--- Options
--- =========================
-vim.opt.termguicolors = true
-vim.g.mapleader = " "
-
--- =========================
--- Plugins
--- =========================
+-- ==========================================================================
+-- PLUGINS
+-- ==========================================================================
 require("lazy").setup({
-  -- Sidebar: NvimTree
+  rocks = { enabled = false, hererocks = false, },
+
+  -- 🎨 Colorscheme
   {
-    "nvim-tree/nvim-tree.lua",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = true,
+    "navarasu/onedark.nvim",
+    priority = 1000,
+    config = function()
+      require("onedark").setup({ style = "dark" })
+      require("onedark").load()
+    end,
   },
 
-  -- Tab/File line: BufferLine
+  -- ✨ Auto pairs {}, (), []
   {
-    "akinsho/bufferline.nvim",
-    dependencies = "nvim-tree/nvim-web-devicons",
-    config = true,
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    config = function()
+      require("nvim-autopairs").setup({
+        check_ts = false,
+      })
+    end,
+  },
+
+  -- 🔍 Telescope
+  {
+    "nvim-telescope/telescope.nvim",
+    cmd = "Telescope",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("telescope").setup({})
+    end,
+  },
+
+  -- 🖱️ Cursor effect (opsional, visual only)
+  {
+    "sphamba/smear-cursor.nvim",
+    opts = {
+      stiffness = 0.5,
+      trailing_stiffness = 0.25,
+      distance_stop_animating = 0.1,
+    },
+  },
+
+  -- 🧠 Autocomplete (CMP)
+  {
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "L3MON4D3/LuaSnip",
+    },
+    config = function()
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<Tab>"] = cmp.mapping.select_next_item(),
+          ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+          ["<C-Space>"] = cmp.mapping.complete(),
+        }),
+        sources = {
+          { name = "nvim_lsp" },
+          { name = "buffer" },
+          { name = "path" },
+        },
+      })
+    end,
+  },
+
+  -- 🐍 LSP Python (Pyright)
+  {
+    "williamboman/mason.nvim",
+    dependencies = {
+      "williamboman/mason-lspconfig.nvim",
+      "hrsh7th/cmp-nvim-lsp",
+    },
+    config = function()
+      require("mason").setup()
+
+      require("mason-lspconfig").setup({
+        ensure_installed = { "pyright" },
+      })
+
+      local capabilities =
+        require("cmp_nvim_lsp").default_capabilities()
+
+      -- Neovim 0.11+
+      vim.lsp.config("pyright", {
+        capabilities = capabilities,
+      })
+    end,
   },
 })
 
--- =========================
--- Keymaps
--- =========================
--- Toggle NvimTree sidebar
-vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
+-- ==========================================================================
+-- LSP KEYMAPS
+-- ==========================================================================
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(ev)
+    local opts = { buffer = ev.buf }
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, opts)
+    vim.keymap.set("n", "<leader>a", vim.lsp.buf.code_action, opts)
+  end,
+})
 
--- Cycle buffers
-vim.keymap.set("n", "<Tab>", ":BufferLineCycleNext<CR>")
-vim.keymap.set("n", "<S-Tab>", ":BufferLineCyclePrev<CR>")
+-- ==========================================================================
+-- TELESCOPE KEYMAPS
+-- ==========================================================================
+vim.keymap.set("n", "<leader>ff", function()
+  require("telescope.builtin").find_files()
+end)
+
+vim.keymap.set("n", "<leader>fg", function()
+  require("telescope.builtin").live_grep()
+end)
+
+vim.keymap.set("n", "<leader>fb", function()
+  require("telescope.builtin").buffers()
+end)
+
+vim.keymap.set("n", "<leader>fh", function()
+  require("telescope.builtin").help_tags()
+end)
+
+-- ==========================================================================
+-- BASIC PYTHON SETTINGS
+-- ==========================================================================
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.completeopt = { "menu", "menuone", "noselect" }
+
